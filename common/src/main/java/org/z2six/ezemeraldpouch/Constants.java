23f4e734f04246cbf22056a4947a837e818c2d3b
Z2SIX <bathofredemption@duck.com>
2026-01-05 12:13:58 +0100

    initial commit, cleaned

---
+++ common/src/main/java/org/z2six/ezemeraldpouch/Constants.java
@@ -0,0 +1,11 @@
+package org.z2six.ezemeraldpouch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "ezemeraldpouch";
+	public static final String MOD_NAME = "EZEmeraldPouch";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
